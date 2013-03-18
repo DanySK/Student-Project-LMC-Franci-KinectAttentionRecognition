a733ceabd9035baf09502ad6c8d5889f42ee0857
sfranci182 <sfranci182@localhost>
2013-03-18 16:26:47 +0100

    Starting 'weightChoice' branch

--- src/control/KinectManager.java
+++ src/control/KinectManager.java
@@ -81,7 +81,6 @@ public class KinectManager {
 	 * @Constructor
 	 */
 	public KinectManager(){
-		//prova
 		LoadParam lp = new LoadParam();
 		lp.readParam(APP_CONFIG_XML);
 		draw = lp.getDraw();
